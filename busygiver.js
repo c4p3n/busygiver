183b949f41661dea016c20bcbdf327877ed45c48
Capen Rhew <capenr@gmail.com>
2015-09-12 17:20:52 -0400

    Initial Commit
    First commit of source code for busygiver.com!

---
+++ busygiver.js
@@ -0,0 +1,73 @@
+// Create database of experiences
+Experiences = new Mongo.Collection("experiences");
+
+
+if (Meteor.isClient) {
+    
+    Meteor.subscribe("experiences");
+    
+    // initialize Session variables
+    Session.setDefault({ 
+        action: "default",
+        city: "default"
+    });
+    
+    // return experiences from database
+    Template.body.helpers({
+        experiences: function () {
+            // user has made no choice of action or city
+            if (Session.equals("action", "default") && Session.equals("city", "default")) {
+                return Experiences.find({});
+            } else if (!(Session.equals("action", "default")) && Session.equals("city", "default")) {
+                // user has only chosen an action
+                return Experiences.find({action: Session.get("action")});
+            } else if (Session.equals("action", "default") && !(Session.equals("city", "default"))) {
+                // user has only chosen a city
+                return Experiences.find({city: Session.get("city")});
+            } else {
+                // user has chosen both an action and a city
+                return Experiences.find({action: Session.get("action"), city: Session.get("city")});
+            }
+        },
+        
+        // fetch public settings
+        publicSettings: function() {
+            return Meteor.settings.public;
+        }
+    });
+    
+    // UNUSED
+    Template.experience.helpers({
+        // convert the ISODate to a user-friendly date format for printing
+        friendlyDateTime: function (datetime) {
+            return datetime.toLocaleString("en-us", { month: "long" }) + " " 
+                + datetime.getDate() + " " 
+                + datetime.getFullYear() + " at "
+                + datetime.toLocaleTimeString("en-us", { hour: "2-digit", minute: "2-digit" });
+        }
+    
+    });
+        
+    Template.body.events({
+        // monitor change of action dropdown
+        "change .action-dropdown": function (event) {
+            Session.set("action", event.target.value);
+            console.log(event);
+        },
+        // monitor change of city dropdown
+        "change .city-dropdown": function (event) {
+            Session.set("city", event.target.value);
+            console.log(event);
+        }
+        
+    });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // publish experiences to client
+    Meteor.publish("experiences", function() {
+        return Experiences.find({});
+    });              
+  });
+}
